5b17d07e8479afcf89582cd5502c69683ad8dea8
TrungNguyen <nguyenhuutrung@Nguyens-MacBook-Air.local>
2023-03-31 14:59:48 +0700

    fourth

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Navbar from './components/Navbar';
-import { HashRouter as Router, Route, Routes} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import './App.css';
 import  Home  from './components/pages/Home.js'
 import Service from './components/pages/Service.js'
