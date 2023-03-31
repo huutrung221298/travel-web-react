2c48eb53bee73e05de8961896770cc3229382fc9
TrungNguyen <nguyenhuutrung@Nguyens-MacBook-Air.local>
2023-03-31 14:51:30 +0700

    third commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Navbar from './components/Navbar';
-import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import { HashRouter as Router, Route, Routes} from 'react-router-dom'
 import './App.css';
 import  Home  from './components/pages/Home.js'
 import Service from './components/pages/Service.js'
@@ -13,10 +13,10 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route path='/' exact element={<Home />} />
-          <Route path='/services' exact element={<Service />} />
-          <Route path='/products' exact element={<Product />} />
-          <Route path='/sign-up' exact element={<SignUp />} />
+          <Route path='/travel-web-react' exact element={<Home />} />
+          <Route path='/travel-web-react/#/services' exact element={<Service />} />
+          <Route path='/#/products' exact element={<Product />} />
+          <Route path='/#/sign-up' exact element={<SignUp />} />
         </Routes>
       </Router>
     </div>
