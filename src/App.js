dbc9805099776db7c7c79532bad36da81334435b
TrungNguyen <nguyenhuutrung@192.168.2.14>
2023-03-31 22:18:56 +0700

    14

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Navbar from './components/Navbar';
-import { HashRouter as Router, Route, Routes} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import './App.css';
 import  Home  from './components/pages/Home.js'
 import Service from './components/pages/Service.js'
@@ -10,10 +10,10 @@ import SignUp from './components/pages/SignUp.js'
 function App() {
   return (
     <div>
-      <Router basename='/travel-web-react'>
+      <Router basename='/'>
         <Navbar />
         <Routes>
-          <Route path='/travel-web-react' exact element={<Home />} />
+          <Route path='/' exact element={<Home />} />
           <Route path='/services'  element={<Service />} />
           <Route path='/products'  element={<Product />} />
           <Route path='/sign-up'  element={<SignUp />} />
