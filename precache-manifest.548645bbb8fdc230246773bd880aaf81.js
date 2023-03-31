098f3ee97f28fd56310bce28d7fe49bcac2f71d2
TrungNguyen <nguyenhuutrung@Nguyens-MacBook-Air.local>
2023-03-31 15:00:49 +0700

    Updates

---
+++ precache-manifest.548645bbb8fdc230246773bd880aaf81.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c808b414ef56f0ea6a9f",
+    "url": "/travel-web-react/static/css/main.776b22ae.chunk.css"
+  },
+  {
+    "revision": "c808b414ef56f0ea6a9f",
+    "url": "/travel-web-react/static/js/main.c808b414.chunk.js"
+  },
+  {
+    "revision": "831e9943f9be5883173d",
+    "url": "/travel-web-react/static/js/1.831e9943.chunk.js"
+  },
+  {
+    "revision": "dc9834920ad791b5474e",
+    "url": "/travel-web-react/static/js/runtime~main.dc983492.js"
+  },
+  {
+    "revision": "3154b722e83b07a13a63880f7991c130",
+    "url": "/travel-web-react/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/travel-web-react/precache-manifest.7fbde2bce377e944caded0ac930b854a.js"
+  "/travel-web-react/precache-manifest.548645bbb8fdc230246773bd880aaf81.js"
 );
 
 workbox.clientsClaim();
