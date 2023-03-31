71bea859f3b258c3246da6e8e732db9b8102b3a7
TrungNguyen <nguyenhuutrung@192.168.2.14>
2023-03-31 21:13:25 +0700

    Updates

---
+++ precache-manifest.c0b96fc07de7a27ef86df00a6878770a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "603b23d32f9aa34afa30",
+    "url": "/travel-web-react/static/css/main.776b22ae.chunk.css"
+  },
+  {
+    "revision": "603b23d32f9aa34afa30",
+    "url": "/travel-web-react/static/js/main.603b23d3.chunk.js"
+  },
+  {
+    "revision": "7c23a64e325add6515b3",
+    "url": "/travel-web-react/static/js/1.7c23a64e.chunk.js"
+  },
+  {
+    "revision": "dc9834920ad791b5474e",
+    "url": "/travel-web-react/static/js/runtime~main.dc983492.js"
+  },
+  {
+    "revision": "fd29675a07b7aad75f67078f87347d39",
+    "url": "/travel-web-react/index.html"
+  }
+];
